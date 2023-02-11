f07cec27ceefa84d8ed353d0422e4bcf95972afe
Przemysław Wojnowski <esperanto@cumego.com>
2023-02-11 11:29:57 +0100

    Display hardcoded list of products.

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'My shopping list'),
+      home: const MyHomePage(title: 'Shopping List'),
     );
   }
 }
@@ -28,7 +28,25 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class Product {
+  String name;
+  bool bought = false;
+  bool deleted = false;
+
+  Product(this.name);
+
+  void buy() {
+    bought = true;
+    print('Bought $name');
+  }
+
+  void delete() {
+    deleted = true;
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Product> _itemsToBuy = [Product("Orzeszki")];
 
   void _addItems() {
     print("Not implemented yet!");
@@ -41,17 +59,51 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text('Add items to buy!'),
-          ],
-        ),
+        child: buildColumnContent(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _addItems,
         tooltip: 'Add',
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget buildColumnContent() {
+    if (_itemsToBuy.isEmpty) {
+      return const Text('Add items to the list!');
+    }
+    return ListView(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 3),
+      children: renderProducts(),
+    );
+  }
+
+  List<Widget> renderProducts() {
+    return _itemsToBuy.map((product) => renderProduct(product)).toList();
+  }
+
+  Widget renderProduct(Product product) {
+    return ListTile(
+      key: ValueKey(product.name),
+      title: Text(product.name),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      tileColor: Colors.lightGreen,
+      //leading: const Icon(Icons.circle, size: 10),
+      trailing: createProductButtons(product),
+    );
+  }
+
+  Widget createProductButtons(Product product) {
+    return SizedBox(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ElevatedButton.icon(
+              onPressed: () => {product.buy()},
+              icon: const Icon(Icons.shopping_basket_outlined),
+              label: const Text(""))
+        ],
       ),
     );
   }
